d078a495edadcf5b98ac7946765e6ed448d1016c
Shireen Ghazala <shireen.nu2022@gmail.com>
2024-12-19 21:38:49 -0600

    Commit again

--- src/showData.java
+++ src/showData.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class showData {
+    public static void main(String[] args) {
+        //Creating object of class AddDataToArrayList
+
+        AddDataToArrayList b = new AddDataToArrayList();
+
+        //Creating ArrayList and storing in it the list returned by b.bookdetails()
+        ArrayList<Book> myBookList = b.bookdetails();
+        for (Book showValue : myBookList) {
+            System.out.println(showValue.getNumber() + " " + showValue.getName() + " " + showValue.getCategory() + " " + showValue.getAuthor());
+        }
+
+
+    }
 }
+
